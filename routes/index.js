24c9ff14e97cac5aed3628a3dd69e2a3134fe391
Taiwo <taiwotriumphant@gmail.com>
2023-03-07 14:18:04 -0800

    simple api tested in ubuntu terminal

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import AppController from "../controllers/AppController";
 const router = express.Router();
 
-router.get('/status', AppController.getStatus())
+router.get('/status', AppController.getStatus)
 
-router.get('/stats', AppController.getStats())
+router.get('/stats', AppController.getStats)
 
 export default router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import router from "./routes/index";
 
-app = express;
+const app = express();
 
 app.use('/', router);
 
