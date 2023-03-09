84866e290bcac86d5186ccd44b658fb45f899231
Taiwo <taiwotriumphnat@gmail.com>
2023-03-09 13:27:25 +0100

    redis client wahala

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -3,13 +3,13 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class FilesController {
-
-    static async postUpload() {
-        const token = req.get('X-Token');
-        // get redis key in redis-server
-        const userId = await redisClient.get(`auth_${token}`);
-        const user = await dbClient.findUser({ _id: ObjectId(userId) });
-    }
+  static async postUpload(req, res) {
+    const token = req.get('X-Token');
+    // get redis key in redis-server
+    const userId = await redisClient.get(`auth_${token}`);
+    const user = await dbClient.findUser({ _id: ObjectId(userId) });
+    res.send(user);
+  }
 }
 
 export default FilesController;
